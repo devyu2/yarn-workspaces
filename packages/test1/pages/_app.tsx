9b18e38969468b37d08b8cca35d4ea50f0a6ef57
devyu2 <hakhyun1110@gmail.com>
2022-05-17 22:07:01 +0900

    불필요 파일 삭제 및 샘플 페이지 추가

--- packages/test1/pages/_app.tsx
+++ packages/test1/pages/_app.tsx
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { HelloWorld } from "test2";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <HelloWorld message="kk" />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ packages/test1/pages/hello-world.tsx
@@ -0,0 +1,7 @@
+import { HelloWorld } from "test2";
+
+const HelloworldPage = () => {
+  return <HelloWorld message="hi" />;
+};
+
+export default HelloworldPage;
